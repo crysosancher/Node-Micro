c485630625022f2ead06867fc9bf00975a797809
crysosancher <vibhupandey.vp@gmail.com>
2022-08-21 12:42:29 +0530

    Posts

---
+++ comments/index.js
@@ -0,0 +1,22 @@
+const express= require ('express');
+
+const bodyParser= require ('body-parser');
+
+const {randomBytes}= require ('crypto');
+
+const app= express();
+app.use(bodyParser.json());
+
+
+const commentsByPostId={};
+
+app.get('/posts/:id/comments', (req, res)=>{
+
+})
+app.post('/posts/:id/comments', (req, res)=>{
+	const commentId=randomBytes(4).toString('hex');
+	
+})
+app.listen(4001, ()=>{
+	    console.log('Server is running on port 4001');
+})
